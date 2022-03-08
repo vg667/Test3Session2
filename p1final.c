b966a26d937a24ab359d6218b270e2201d9bdc40
Vinaya001 <93110662+vg667@users.noreply.github.com>
2022-03-08 05:00:52 +0000

    nice

--- p1final.c
+++ p1final.c
@@ -1,1 +1,46 @@
 #include <stdio.h>
+void input(int *num1,int *den1,int *num2,int *den2)
+{
+printf("Enter numerator and denominator value ");
+scanf("%d%d",num1,den1);
+printf("Enter numerator and denominator value ");
+scanf("%d%d",num2,den2);
+}
+
+
+
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+int hcf,lcm,a,b;
+for (int i=1;i<=den1 && i<=den2;++i)
+{
+if (den1%i==0 && den2%i==0)
+{
+hcf=i;
+}
+}
+lcm=(den1 * den2)/hcf;
+a=lcm/den1;
+b=lcm/den2;
+num1=a*num1;
+den1=a*den1;
+num2=b*num2;
+den2=b*den2;
+*num3=num1+num2;
+*den3=den1;
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+printf("%d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+}
+
+
+
+int main()
+{
+int num1,den1,num2,den2,num3,den3;
+input(&num1,&den1,&num2,&den2);
+add(num1,den1,num2,den2,&num3,&den3);
+output(num1,den1,num2,den2,num3,den3);
+return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,54 @@
 #include<stdio.h>
+typedef struct fraction
+{
+int num,den;
+}fraction;
+fraction input_fraction()
+{
+fraction a;
+printf("Enter numerator and denominator value ");
+scanf("%d%d",&a.num,&a.den);
+return a;
+}
+
+
+
+fraction largest(fraction f1,fraction f2,fraction f3)
+{
+int a,b,c;
+a=f1.num/f1.den;
+b=f2.num/f2.den;
+c=f3.num/f3.den;
+if (a>b && a>c)
+{
+return f1;
+}
+else if (b>a && b>c)
+{
+return f2;
+}
+else
+{
+return f3;
+}
+}
+
+
+
+void output(fraction largest)
+{
+printf("%d/%d is the largest",largest.num,largest.den);
+}
+
+
+
+int main()
+{
+fraction f1,f2,f3,l;
+f1=input_fraction();
+f2=input_fraction();
+f3=input_fraction();
+l=largest(f1,f2,f3);
+output(l);
+return 0;
+}
